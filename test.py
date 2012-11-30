1ffd447290d479c344de6ab1ea48f1e5bb103e45
berryman <alexbman@gmail.com>
2012-11-29 22:17:09 -0500

    tests for url parsing

--- test.py
+++ test.py
@@ -37,12 +37,13 @@
 #Py.test tests by Bear
 from plugin import Imgur
 
+url_list = [
+    {"type": "album", "url": "http://imgur.com/a/q1WIO", "hash": "q1WIO"},
+    {"type": "gallery", "url": "http://imgur.com/gallery/XFegc", "hash": "XFegc"},
+    {"type": "image", "url": "http://i.imgur.com/jgONW.jpg", "hash": "jgONW"}
+]
+
 def test_imgur_parse_url():
-    url_list = [
-        {"type": "album", "url": "http://imgur.com/a/q1WIO", "hash": "q1WIO"},
-        {"type": "gallery", "url": "http://imgur.com/gallery/XFegc", "hash": "XFegc"},
-        {"type": "image", "url": "http://i.imgur.com/jgONW.jpg", "hash": "jgONW"}
-        ]
     for imgur_dict in url_list:
         imgur_hash = Imgur._parse_url(imgur_dict["url"])
         assert imgur_dict["hash"] == imgur_hash
